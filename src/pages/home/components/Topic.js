d89a98a25b68e23339468a2a968e6b908d6bf37b
kkxxll <550893685@qq.com>
2018-08-19 15:13:29 +0800

    8.4

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,11 +1,33 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { TopicWrapper, TopicItem } from '../style';
 
 class Topic extends Component {
-    render() {
-        return(
-            <div>Topic</div>
-        )
-    }
+  render() {
+    const {list} = this.props
+    return (
+      <TopicWrapper>
+        {list.map(item => (
+            <TopicItem key={item.get('id')}>
+              <img
+                className="topic-pic"
+                src={item.get('imgUrl')}
+                alt=""
+              />
+              {item.get('title')}
+            </TopicItem>
+          ))}
+      </TopicWrapper>
+    );
+  }
 }
 
-export default Topic
+const mapState = state => ({
+  list: state.get('home').get('topicList')
+});
+
+export default connect(
+  mapState,
+  null
+)(Topic);
---
+++ src/pages/home/store/reducer.js
@@ -0,0 +1,37 @@
+import { fromJS } from 'immutable';
+
+const defaultState = fromJS({
+  topicList: [{
+    id: 1,
+    title: 'sdfd',
+    imgUrl: '//upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540'
+  },{
+    id: 2,
+    title: '22sdfd',
+    imgUrl: '//upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540'
+  }],
+  articleList: [{
+    id: 1,
+    title: 'sfjlds',
+    desc: 'sdfdsffds',
+    imgUrl: '//upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540'
+  },
+  {
+    id: 2,
+    title: 'sfjlds',
+    desc: 'sdfdsffds',
+    imgUrl: '//upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540'
+  },{
+    id: 3,
+    title: 'sfjlds',
+    desc: 'sdfdsffds',
+    imgUrl: '//upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540'
+  }]
+});
+
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
