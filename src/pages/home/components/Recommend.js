79218f95d10c853d6aa422e62dfed9866901cc62
kkxxll <550893685@qq.com>
2018-08-19 15:28:22 +0800

    8.5

--- src/pages/home/components/Recommend.js
+++ src/pages/home/components/Recommend.js
@@ -1,11 +1,27 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+
+import { RecommendItem, RecommendWrapper } from '../style';
 
 class Recommend extends Component {
-    render() {
-        return(
-            <div>Recommend</div>
-        )
-    }
+  render() {
+    const { list } = this.props;
+    return (
+      <RecommendWrapper>
+        {list.map(item => (
+          <RecommendItem imgUrl={item.get('imgUrl')} key={item.get('id')} />
+        ))}
+      </RecommendWrapper>
+    );
+  }
 }
 
-export default Recommend
+const mapState = state => ({
+  list: state.getIn(['home', 'recommendList'])
+});
+
+export default connect(
+  mapState,
+  null
+)(Recommend);
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -26,6 +26,13 @@ const defaultState = fromJS({
     title: 'sfjlds',
     desc: 'sdfdsffds',
     imgUrl: '//upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540'
+  }],
+  recommendList: [{
+    id: 1,
+    imgUrl: '//upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540'
+  },{
+    id: 2,
+    imgUrl: '//upload.jianshu.io/admin_banners/web_images/4318/60781ff21df1d1b03f5f8459e4a1983c009175a5.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540'
   }]
 });
 
